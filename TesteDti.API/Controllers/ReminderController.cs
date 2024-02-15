5623aead3a40748bfece7274f560001b67572b29
Bruna  Rolim <brunaerrolim@gmail.com>
2024-02-14 21:59:06 -0300

    Create Methods

--- TesteDti.API/Controllers/ReminderController.cs
+++ TesteDti.API/Controllers/ReminderController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TesteDti.API.Entities;
 using TesteDti.API.Persistence;
 
 namespace TesteDti.API.Controllers
@@ -23,6 +24,60 @@ namespace TesteDti.API.Controllers
             var reminders = _context.Reminder.Where(d => !d.IsDeleted).ToList();
 
             return Ok(reminders);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var reminders = _context.Reminder.SingleOrDefault(d => d.Id == id);
+
+                if (reminders == null) 
+                {
+                    return NotFound();
+                
+                }
+
+            return Ok(reminders);
+
+        }
+
+        [HttpPost]
+        public IActionResult Post(Reminder reminders)
+        {
+            _context.Reminder.Add(reminders);
+
+            return CreatedAtAction(nameof(GetById), new { id = reminders.Id }, reminders);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id, Reminder input)
+        {
+            var reminders = _context.Reminder.SingleOrDefault( d =>  d.Id == id);
+
+            if (reminders == null)
+            {
+                return NotFound();
+            }
+
+            reminders.Update(input.Name, input.Date);
+
+            return NoContent();
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var reminders = _context.Reminder.SingleOrDefault(d => d.Id == id);
+
+                if (reminders == null)
+                {
+                    return NotFound();
+                }
+
+                reminders.Delete();
+
+            return NoContent();
         }
     }
 }
--- TesteDti.API/Entities/Reminder.cs
+++ TesteDti.API/Entities/Reminder.cs
@@ -12,5 +12,17 @@
         public DateTime Date { get; set; }
         public bool IsDeleted { get; set; }
 
+        public void Update(string name, DateTime date)
+        {
+            Name = name;
+            Date = date;
+
+        }
+
+        public void Delete()
+        {
+            IsDeleted = true;
+        }
+
     }
 }
